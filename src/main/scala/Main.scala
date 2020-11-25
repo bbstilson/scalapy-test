e872f65d78c98eedf1b847b9967ab9d55ea5501f
Brandon Stilson <bbstilson@fastmail.com>
2020-11-25 08:18:37 -0800

    clean

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,9 @@
 import me.shadaj.scalapy.py
 import me.shadaj.scalapy.py.SeqConverters
 
-import scala.io._
-
 object Main {
-  // val modelBytes: BufferedSource = Source.fromResource("trained_ranker_model_minimal.pickle.tar.gz")
-  // val pickle = py.module("pickle")
 
   def main(args: Array[String]): Unit = {
-    // println(pickle)
-
     val listLengthPython = py.Dynamic.global.len(List(1, 2, 3).toPythonProxy)
     println(listLengthPython)
   }
